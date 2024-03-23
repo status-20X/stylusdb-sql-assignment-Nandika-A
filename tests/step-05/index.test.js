4d01431439a9884f4ee0e02d0b0a72a8c9fcb395
Nandika-A <nandikaagrawal610@gmail.com>
2024-03-23 14:35:55 +0530

    test 5

--- tests/step-05/index.test.js
+++ tests/step-05/index.test.js
@@ -36,7 +36,11 @@ test('Parse SQL Query with WHERE Clause', () => {
     expect(parsed).toEqual({
         fields: ['id', 'name'],
         table: 'sample',
-        whereClause: 'age = 25'
+        whereClauses : [{
+            "field": "age",
+            "operator": "=",
+            "value": "25",
+            },],
     });
 });
 
